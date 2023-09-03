35b9772f90622797ba43977215aaeb97419d3eba
Daria Shcherbinina <sda@nova-sphere.ru>
2023-09-03 23:44:53 +0500

    Домашнее задание - Управление потоком.

---
+++ 5-languages/script.js
@@ -0,0 +1,18 @@
+/*
+    Выводим приветствие пользователю в зависимости от переданного языка
+ */
+
+let lang = 'de';
+
+switch (lang) {
+    case 'ru':
+        console.log('Привет!');
+        break;
+    case 'en':
+        console.log('Hello!');
+        break;
+    case 'de':
+        console.log('Gutten tag!');
+        break;
+}
+
